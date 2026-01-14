1cf162a19cd2b4fc03ae2f2a15d107c5805f4f9b
xon_patrick <132488506+xon-patrick@users.noreply.github.com>
2026-01-14 09:20:55 +0200

    latest trained model in onnx

--- src/postprocessing/to_onnx.py
+++ src/postprocessing/to_onnx.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from ultralytics import YOLO
 
-model_path = Path(__file__).parent.parent.parent / "trained_models" / "model_003_best.pt"
+model_path = Path(__file__).parent.parent.parent / "trained_models" / "model_005_best.pt"
 
 model = YOLO(str(model_path))
 
